02e331af927cdee4579e92a6e57450e5a8917f5a
alexsandro-matias <matiasalexsandro@gmail.com>
2022-07-11 16:20:17 -0300

    start from scratch

--- hangman.py
+++ hangman.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-
 # Hangman Game (Jogo da Forca)
 # Programação Orientada a Objetos
 
@@ -70,63 +69,54 @@ O   |
 =========''']
 
 
-# Classe
 class Hangman:
 
-	# Método Construtor
-	def __init__(self, word):
-        
-		
-		
-	# Método para adivinhar a letra
-	def guess(self, letter):
-        
-		
-		
-	# Método para verificar se o jogo terminou
-	def hangman_over(self):
-		
-		
-	# Método para verificar se o jogador venceu
-	def hangman_won(self):
-		
+    def __init__(self, word):
+        self.word = word
 
-	# Método para não mostrar a letra no board
-	def hide_word(self):
-		
-		
-	# Método para checar o status do game e imprimir o board na tela
-	def print_game_status(self):
-		
+    # Método para adivinhar a letra
+    def guess(self, letter):
+        pass
+
+    def hangman_over(self):
+        pass
+
+    def hangman_won(self):
+        pass
+
+    def hide_word(self):
+        quantidade_letras = len(self.word)
+
+    def print_game_status(self):
+        pass
+
 
 # Função para ler uma palavra de forma aleatória do banco de palavras
 def rand_word():
-        with open("palavras.txt", "rt") as f:
-                bank = f.readlines()
-        return bank[random.randint(0,len(bank))].strip()
+    with open("words.txt", "rt") as f:
+        bank = f.readlines()
+    return bank[random.randint(0, len(bank))].strip()
 
 
 # Função Main - Execução do Programa
 def main():
+    game = Hangman(rand_word())
 
-	# Objeto
-	game = Hangman(rand_word())
+    # Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
 
-	# Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
-	
+    # Verifica o status do jogo
+    game.print_game_status()
 
-	# Verifica o status do jogo
-	game.print_game_status()	
+    # De acordo com o status, imprime mensagem na tela para o usuário
+    if game.hangman_won():
+        print('\nParabéns! Você venceu!!')
+    else:
+        print('\nGame over! Você perdeu.')
+        print('A palavra era ' + game.word)
 
-	# De acordo com o status, imprime mensagem na tela para o usuário
-	if game.hangman_won():
-		print ('\nParabéns! Você venceu!!')
-	else:
-		print ('\nGame over! Você perdeu.')
-		print ('A palavra era ' + game.word)
-		
-	print ('\nFoi bom jogar com você! Agora vá estudar!\n')
+    print('\nFoi bom jogar com você! Agora vá estudar!\n')
 
-# Executa o programa		
+
+# Executa o programa
 if __name__ == "__main__":
-	main()
+    main()
